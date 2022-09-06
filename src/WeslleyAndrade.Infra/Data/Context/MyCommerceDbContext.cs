a14480a73745886b4a786a45e81efe5356419c99
Weslley Andrade <weslleyp.andrade@gmail.com>
2022-09-06 08:59:45 +0100

    Camada de Infra

---
+++ src/WeslleyAndrade.Infra/Data/Context/MyCommerceDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using WeslleyAndrade.Business.Models.Fornecedores;
+using WeslleyAndrade.Business.Models.Produtos;
+using WeslleyAndrade.Infra.Data.Mappings;
+
+namespace WeslleyAndrade.Infra.Data.Context
+{
+    public class MyCommerceDbContext : DbContext
+    {
+        public MyCommerceDbContext() : base("name=DefaultConnection")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p
+               .HasColumnType("varchar")
+               .HasMaxLength(100));
+
+            modelBuilder.Configurations.Add(new FornecedorConfig());
+            modelBuilder.Configurations.Add(new EnderecoConfig());
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
